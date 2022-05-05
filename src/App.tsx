4046e44970c4a7035f6458b247217a49cc5a9603
Lucas Gomes <lucas.sist.inf@gmail.com>
2022-05-05 12:57:51 -0300

    configured environments (dev, staging)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import './App.css';
-import { AccessTime } from '@mui/icons-material';
-import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" target="_blank" rel="noopener noreferrer">
-          Learn React <AccessTime sx={{ fontSize: 50 }} />
-          <Button variant="contained">Acessar</Button>
-        </a>
+        <span>Ambiente: {process.env.REACT_APP_ENVIRONMENT}</span>
       </header>
     </div>
   );
