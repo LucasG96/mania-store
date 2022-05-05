a87b089906ca8d3b87ea21c2dfed56647b44176c
Lucas Gomes <lucas.sist.inf@gmail.com>
2022-05-05 13:56:38 -0300

    Revert "configured environments (dev, staging)"
    This reverts commit 4046e44970c4a7035f6458b247217a49cc5a9603.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import { AccessTime } from '@mui/icons-material';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <span>Ambiente: {process.env.REACT_APP_ENVIRONMENT}</span>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" target="_blank" rel="noopener noreferrer">
+          Learn React <AccessTime sx={{ fontSize: 50 }} />
+          <Button variant="contained">Acessar</Button>
+        </a>
       </header>
     </div>
   );
