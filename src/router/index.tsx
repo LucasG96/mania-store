dadadb661a0e2ae93d1c65b860cc737bcf4719f8
Lucas Gomes <lucas.sist.inf@gmail.com>
2022-05-05 20:30:05 -0300

    fixed redirect

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import PageNotFound from '../pages/NotFound';
 import ProductModule from '../pages/Product';
 import ShoppingModule from '../pages/Shopping';
+import { ApplicationRoutes } from '../shared/utils/enum';
 
 function Router() {
   const routes = [...ProductModule, ...ShoppingModule];
@@ -13,6 +14,10 @@ function Router() {
         {routes.map((route) => (
           <Route key={route.path} path={route.path} element={route.component} />
         ))}
+        <Route
+          path="/"
+          element={<Navigate to={ApplicationRoutes.ProdutList} />}
+        />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
